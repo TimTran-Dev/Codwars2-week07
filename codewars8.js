cd8140be0c3af9b1bd484a072fea3371eb84f2fe
TimTran-Dev <55710300+TimTran-Dev@users.noreply.github.com>
2019-10-29 13:33:33 -0400

    Add files via upload

---
+++ codewars8.js
@@ -0,0 +1,16 @@
+// Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+//
+// To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
+
+
+function add(a, b){
+  let number = a;
+  let string = b;
+  if(number == string){
+    return true;
+  }else{
+    return false;
+  }
+  number = 10;
+  string = "hi"
+}
