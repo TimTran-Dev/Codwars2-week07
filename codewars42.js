8ae16bb04fbaab87258f5c959b32fc6c67e86de6
TimTran-Dev <55710300+TimTran-Dev@users.noreply.github.com>
2020-03-06 23:48:03 -0500

    Add files via upload

---
+++ codewars42.js
@@ -0,0 +1,18 @@
+var lengthOfLongestSubstring = function(s) {
+  let hashMap = {},
+    start = 0,
+    max = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (hashMap[s[i]] != null) {
+      if (hashMap[s[i]] + 1 > start) {
+        // if the character is after start
+        start = hashMap[s[i]] + 1; // update start position
+      }
+    }
+    hashMap[s[i]] = i; // update char position
+    max = i - start + 1 > max ? i - start + 1 : max; // update max if neccessary
+  }
+  console.log(hashMap);
+  return max;
+};
